4d6f25cc0fb67ee1ca562e8367535a2ce0778adf
Hemanth Doddala <hemanth@Hemanths-MacBook-Pro.local>
2025-09-16 18:09:26 -0500

    Randomize css classname

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import {Switch, Route, BrowserRouter as Router} from "react-router-dom";
-import { StylesProvider} from '@material-ui/core/styles';
+import { StylesProvider,createGenerateClassName} from '@material-ui/core/styles';
 import Pricing from './components/Pricing';
 import Landing from './components/Landing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+});
+
 export default ()=>{
   return (<div>
-    <StylesProvider>
+    <StylesProvider generateClassName={generateClassName}>
       <Router>
         <Switch>
           <Route  path='/' component={Landing}/>
