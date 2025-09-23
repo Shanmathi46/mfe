3db4fa8aef051bc111b28394c72f5f4de941c607
Hemanth Doddala <hemanth@Hemanths-MacBook-Pro.local>
2025-09-23 15:01:03 -0500

    Added Authentication

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import { createMemoryHistory ,createBrowserHistory} from 'history';
 import App from './App';
 
-const mount=(el,{onNavigate,defaultHistory})=>{
-  const history=defaultHistory || createMemoryHistory();
+const mount=(el,{onSignIn, onNavigate,defaultHistory,initialPathname})=>{
+  const history=defaultHistory || createMemoryHistory(
+    {
+      initialEntries:[initialPathname]
+    }
+  );
   if(onNavigate){
   history.listen(onNavigate)
   }
-  ReactDOM.render(<App history={history}/>,el)  
+  ReactDOM.render(<App onSignIn={onSignIn} history={history}/>,el)  
   return{
     onParentNavigate({pathname:nextPathname}){
       const {pathname}=history.location;
--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React,{lazy, Suspense,useState} from 'react';
 import {StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 import Header from './components/Header';
-import {BrowserRouter} from 'react-router-dom';
+const AuthApp = lazy(()=> import('./components/AuthApp'));
+const MarketingApp = lazy(()=> import('./components/MarketingApp'));
+import Progress from './components/Progress';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co'
 });
 
 export default ()=>{
+  const [isSignedIn, setIsSignedIn] = useState(false);
   return (
       <BrowserRouter>
         <StylesProvider generateClassName={generateClassName}>
           <div>
-            <Header/>
-            <MarketingApp/>
+            <Header onSignOut={()=>setIsSignedIn(false)} isSignedIn={isSignedIn}/>
+            <Suspense fallback={<Progress/>}>
+              <Switch>
+                <Route path='/auth' >
+                <AuthApp onSignIn={()=> setIsSignedIn(true)}/>
+                </Route>
+                <Route path='/' component={MarketingApp}/>
+              </Switch>
+            </Suspense>
           </div>
           </StylesProvider>
       </BrowserRouter> 
